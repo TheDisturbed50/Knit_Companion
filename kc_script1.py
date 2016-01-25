5f7f534ff4c70b3d4b4980ac2f260f7ff3a2a0d9
T.J. Calhoun <john.fullmetaljacket@gmail.com>
2016-01-25 00:04:27 -0900

    rev_2

--- kc_script1.py
+++ kc_script1.py
@@ -1,15 +1,17 @@
-# rev 0.01 alpha
+# rev 0.02 alpha
 # Python Build 3.5.1
 # Author: Thomas Calhoun    john.fullmetaljacket@gmail.com
 
-print('''
->Welcome to "TheDisturbed50\'s Knit Companion"!
->You are in the Command-Prompt edition, ver. 0.02alpha
->Please follow the prompts and bear with me on the user
-   input requirements as I work on possible workarounds.
+print('''Welcome to "TheDisturbed50\'s Knit Companion"!
+
+You are in the Command-Prompt edition, ver. 0.02alpha
+
+Please follow the prompts and bear with me on the user
+input requirements as I work on possible workarounds.
+
    Thank you!
-      ~Thomas Calhoun \n
-''')
+      ~Thomas Calhoun \n''')
+
 print("        ***Patern Input***")
 print('''Please note: if your pattern increases rows,
  break down the input into "Phases" as you complete it.\n''')
@@ -17,18 +19,20 @@ TotalRows = int(input("Enter the total number of rows     -->"))
 TotalRepeats = int(input("Enter the total number of repeats  -->"))
 CurRows = 0
 CurRepeats = 0
+TotalRowComp = TotalRows+1
+TotalRepeatComp = TotalRepeats+1
 
-while CurRepeats < TotalRepeats:
+while CurRepeats < TotalRepeatComp:
     print("...Currently %d of %d Rows" % (CurRows,TotalRows))
     print("...Currently %d of %d Repeats" % (CurRepeats,TotalRepeats))
     if int(input("Enter any number, then press Enter for Row Complete...\n")) < 10:
         CurRows += 1
-        if CurRows == TotalRows:
+        if CurRows == TotalRowComp:
             CurRepeats += 1
             CurRows = 0
             print(">>>Good Work! Next Row!\n\n")
-            if CurRepeats == TotalRepeats:
-                print("/n/n~*/Good Job! Pattern Complete!\\*~")
+            if CurRepeats == TotalRepeatComp:
+                print("\n\n~*/Good Job! Pattern Complete!\\*~")
                 input("Press enter to exit...")
             else:
                 continue
