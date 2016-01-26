f0ae970d861926f698a775d1da19692560b52d57
T.J. Calhoun <john.fullmetaljacket@gmail.com>
2016-01-25 23:34:21 -0900

    Rev_2 (non-working) GUI

--- kc-gui_script1.py
+++ kc-gui_script1.py
@@ -2,35 +2,45 @@
 # Python Build 3.5.1
 # Author: Thomas Calhoun    john.fullmetaljacket@gmail.com
 
-#!/usr/bin/env python
 from tkinter import *
+import tkinter.messagebox as tkmb
 
+root = Tk()
+root.geometry("500x200")
 
 TotalRows = StringVar()
 TotalRepeats = StringVar()
+
 CurRows = 0
 CurRepeats = 0
+
+def TotalRowsWritten(*args):
+    print("TotalRowsWritten",TotalRows.get())
+def TotalRepeatsWritten(*args):
+    print("TotalRepeatsWritten",TotalRepeats.get())
+
+TotalRowComp = TotalRows
+TotalRepeatComp = TotalRepeats
 
 class Window(Frame):
     def __init__(self, master = None):
         Frame.__init__(self, master)
         self.master = master
         self.mainWindow()
-        
+
     def mainWindow(self):
         self.master.title("Knit Companion")
         self.pack(fill=BOTH, expand=1)
         quitButton = Button(self, text="Exit", command=self.quit)
         quitButton.place(x=0, y=80)
-
+        TotalRowE = Entry(root, width=4, textvariable=TotalRows)
+        TotalRowE.place(x=195, y=25)
+        TotalRepeatE = Entry(root, width=4, textvariable=TotalRepeats)
+        TotalRepeatE.place(x=195, y=50)
         TotRowLab = Label(self, text='Please enter the total # of Rows')
         TotRowLab.place(x=0, y=25)
         TotRptLab = Label(self, text='Please enter the total # of Repeats')
         TotRptLab.place(x=0, y=50)
-        TotRowEnt = Entry(root, width=4, textvariable=TotalRows)
-        TotRowEnt.place(x=195, y=25)
-        TotRptEnt = Entry(root, width=4, textvariable=TotalRepeats)
-        TotRptEnt.place(x=195, y=50)
         TotRowPrint = Label(root, fg="red", textvariable=TotalRows)
         TotRowPrint.place(x=340, y=25)
         TotRptPrint = Label(root, fg="red", textvariable=TotalRepeats)
@@ -39,19 +49,12 @@ class Window(Frame):
         ofRowsLab.place(x=320, y=25)
         ofRptLab = Label(self, text='of        Total Repeats')
         ofRptLab.place(x=320, y=50)
-
-
-mButton1 = Button(text = CurRows, command = aClick, fg = "black", bg = "white")
-mButton1.place(x=280, y=22)
-mButton2 = Button(text = CurRepeats, command = bClick, fg = "black", bg = "white")
-mButton2.place(x=280, y=48)
-
-def TotalRowsWritten(*args):
-    print("TotalRowsWritten",TotalRows.get())
-def TotalRepeatsWritten(*args):
-    print("TotalRepeatsWritten",TotalRepeats.get())
-TotalRows.trace("w", TotalRowsWritten)
-TotalRepeats.trace("w", TotalRepeatsWritten)
+        global mButton1
+        mButton1 = Button(text = CurRows, command = aClick, fg = "black", bg = "white")
+        mButton1.place(x=280, y=22)
+        global mButton2
+        mButton2 = Button(text = CurRepeats, command = bClick, fg = "black", bg = "white")
+        mButton2.place(x=280, y=48)
 
 def aClick():
     global CurRows
@@ -62,14 +65,21 @@ def bClick():
     CurRepeats += 1
     mButton2.config(text = CurRepeats)
 
-while CurRepeats < TotalRepeats:
-    if CurRows == TotalRows:
-        CurRepeats += 1
-        CurRows = 0
-    else:
-        finLab = Label(app, text='Good Job! Pattern Complete!')
-        finLab.place(x=200, y=90)
+TotalRows.trace("w", TotalRowsWritten)
+TotalRepeats.trace("w", TotalRepeatsWritten)
 
-root = Tk
+#TODO: fix conditional error from StrVar() variables above to INT or reconfigure
+#TODO> the variable entry altogether. Remaining code deactivated until fixed.
+#TODO: bind keystroke <space> as conditional for "if" statement.
+#while CurRepeats < TotalRepeatComp:
+#    if aClick():
+#        CurRows += 1
+#    if CurRows == TotalRowComp:
+#        CurRepeats += 1
+#        CurRows = 0
+#        if CurRepeats == TotalRepeatComp:
+#                if tkmb.askokcancel(title="Close App", message="Pattern Complete\nWould you like to exit?"):
+#                    root.destroy()
+
 app = Window(root)
 root.mainloop()
